aa59daef6ba28f8a740b86e609919b55e4cdfae5
khiemdang111 <dangkhiemct111@gmail.com>
2025-03-22 05:02:52 +0700

    edit param product detail client

--- app/Http/Controllers/Client/ProductController.php
+++ app/Http/Controllers/Client/ProductController.php
@@ -7,8 +7,8 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    public function detail(){
+    public function detail($slug){
         // Your logic to retrieve product details goes here
-        return view('client.product.detail');
+        return view('client.product.detail', compact('slug'));
     }
 }
--- resources/views/Client/product/detail.blade.php
+++ resources/views/Client/product/detail.blade.php
@@ -1,7 +1,7 @@
 @extends('client.layout.master')
 @section('content')
     <div>
-        <h1>Chi tiết sản phẩm!</h1>
+        <h1>Chi tiết sản phẩm {{ $slug }}! </h1>
     </div>
 @endsection
 @section('meta-title')
